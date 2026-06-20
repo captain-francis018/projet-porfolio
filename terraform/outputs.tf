d46b5d848aed7c5c2436a58e72d41931c272f7d5
abdoukarimsy018 <abdoukarimsy018@gmail.com>
2026-06-20 17:39:28 +0000

    correction

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "frontend_url" {
   description = "url d'acces au frontend"
-  value       = "http://192.168.30.20:30080"
+  value       = "http://192.168.30.10:30080"
 }
 output "backend_url" {
   description = "service backend k8s"
